daa73e9a4e11a40b334b9cc03f3e5faa77d74b7c
Aaron Decker <aaron@stronganchortech.com>
2022-06-04 10:40:03 -0500

    Add proof-of-concept page generation.

---
+++ src/math_drill/print.clj
@@ -0,0 +1,45 @@
+(ns math-drill.print
+  (:require [math-drill.gen :as gen]))
+
+(defn preamble []
+  (str
+   "\\documentclass[11pt]{article}
+\\usepackage{multicol}
+\\usepackage{fancyhdr}
+\\author{aaron}
+\\date{\\today}
+\\title{}
+
+\\begin{document}
+
+\\pagestyle{fancy}
+\\fancyhead{}
+\\lhead{\\textit{Math Drill}}
+\\rhead{\\textit{Simple Linear Equations}}
+
+"))
+
+(defn exercises-section [exercises]
+  (str
+   "\\begin{multicols}{3}
+  \\begin{enumerate}
+"
+   (apply str (map (fn [{:keys [question] :as exercise}]
+                     (str "    \\item{" question "}\n"))
+                   exercises))
+   "  \\end{enumerate}
+\\end{multicols}
+"))
+
+(defn postamble []
+  "\\end{document}")
+
+(defn write-page! []
+  (let [filename "data/sample.tex"
+        num-exercises 30
+        exercises (take num-exercises (repeatedly gen/simple-linear-equation))]
+    (spit filename
+          (str
+           (preamble)
+           (exercises-section exercises)
+           (postamble)))))
