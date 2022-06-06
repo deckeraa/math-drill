a48461d71af3145fe96fc4c5c2ff9f09b38ff4d7
Aaron Decker <aaron@stronganchortech.com>
2022-06-06 16:03:22 -0500

    Add checkbox for half-page.

--- src/math_drill/print_ui.cljs
+++ src/math_drill/print_ui.cljs
@@ -7,6 +7,12 @@
 (defn print-ui-core []
   (r/with-let [opts-atom (r/atom {:half-page? false})]
     [:div
+     [:ul
+      [:li
+       [:input {:type :checkbox
+                :value (:half-page? @opts-atom)
+                :on-change #(swap! opts-atom (fn [m] (update m :half-page? not)))}]
+       "Use half-page format"]]
      [:a {:href (str util/data-url "/download-exercise-sheet?opts=" (pr-str @opts-atom))
           :target "_blank"}
       "Generate exercise sheet"]]))
